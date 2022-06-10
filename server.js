347d75fda1bfe56f926a59d9c8d80a4f2179b26c
marcos.castro <marcos.castro@f13.mail>
2022-06-10 20:26:15 -0300

    Novo

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
 const appName = 'levantti-cred';
 
+app.use(requireHTTPS);
 app.use(express.static(__dirname + `/dist/${appName}`));
 
 app.get('/*', function (req, res) {
